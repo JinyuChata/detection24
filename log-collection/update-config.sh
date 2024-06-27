87b58c250b16a9a5f3ee2633c6956d7f12e59bab
Jinyu Zhu <zhujy22@mails.tsinghua.edu.cn>
2024-06-27 14:28:56 +0800

    auto generate erinyes config

--- log-collection/update-config.sh
+++ log-collection/update-config.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 pods_info=$(kubectl get pods -A -o wide | grep purchase)
+conf_sample_file="erinyes-code/conf/config.yaml.sample"
+conf_file="erinyes-code/conf/config.yaml"
 
-echo "$pods_info" | while read -r line; do
-  # IP
+echo "# Auto generated from config.yaml.sample." > $conf_file
+echo "# DO NOT EDIT" >> $conf_file
+cat $conf_sample_file >> $conf_file
+
+# IP
+ip_map=""
+while read -r line; do
   ip=$(echo "$line" | awk '{print $7}')
-  # POD
   name=$(echo "$line" | awk '{print $2}')
-  
-  # POD_ID
   id=$(echo "$line" | awk '{print $2}' | awk -F'-' '{print $(NF-1)"-"$NF}')
   formatted_name=$(echo "$name" | sed "s/-$id//")
-  echo "  $ip: $formatted_name\$$id"
-done
+  ip_map+="  $ip: $formatted_name\$$id\n"
+done <<< "$pods_info"
+
+ip_map=$(echo "$ip_map" | sed '$s/\\n$//')
+
+sed -i "s|<IPMAP>|$ip_map|g" "$conf_file"
+
+# GATEWAY
+gateway_info=$(kubectl get svc -o wide -A | grep gateway)
+gateway_map=""
+while read -r line; do
+  ip=$(echo "$line" | awk '{print $4}')
+  gateway_map+="  $ip: true\n"
+done <<< "$gateway_info"
+gateway_map=$(echo "$gateway_map" | sed '$s/\\n$//')
+sed -i "s|<GATEWAYMAP>|$gateway_map|g" "$conf_file" >> "$conf_file"
